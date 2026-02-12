4808391e66ca6c7ab03368bba3384ba9c92f04b7
qq542vev <qq542vev@yahoo.co.jp>
2026-02-12 22:44:13 +0900

    fix: Webと時限実行のみ許可。

--- automation.sh
+++ automation.sh
@@ -5,13 +5,14 @@ set -eu
 git config user.name 'qq542vev'
 git config user.email 'qq542vev@yahoo.co.jp'
 
-git checkout -B automation
+git checkout -B automation origin/automation
+git checkout origin/master -- makefile
 
 make all publish
 
 find build -type f ! -empty -exec sh -euc 'for f in "${@}"; do : >"${f}"; done' sh '{}' +
 
 git status --porcelain | grep . || exit 0
-git add -A
+git add build
 git commit -m 'automation: mark processed files'
-git push "https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.com/q542vev/newmoon-sfs.git" 'HEAD:automation'
+git push "https://token:${GITLAB_TOKEN}@gitlab.com/q542vev/newmoon-sfs.git" 'HEAD:automation'
