7e4abffce0a461270af3b64108a2c797663a1a1c
qq542vev <qq542vev@yahoo.co.jp>
2026-02-13 23:56:48 +0900

    automation.sh を編集

--- automation.sh
+++ automation.sh
@@ -12,6 +12,9 @@ git branch -a
 git checkout -B automation origin/automation
 git checkout origin/master -- GNUmakefile rootfs
 
+echo "RCLONE_CONFIG: $RCLONE_CONFIG"
+ls -la $RCLONE_CONFIG
+
 make SHELL=bash build/newmoon-3dnow/debain8gcc7+newmoon3dnow-32.3.1.linux-i586-gtk2.xz.sfs publish
 
 find "${DIR}" -type f ! -size 0 -exec sh -euc 'for f in "${@}"; do : >"${f}"; done' sh '{}' +
