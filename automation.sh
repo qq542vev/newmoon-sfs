654502e90217b4ae672d4d301770c29c0ec4d6b7
qq542vev <qq542vev@yahoo.co.jp>
2026-02-14 16:23:19 +0900

    automation.sh を編集

--- automation.sh
+++ automation.sh
@@ -13,7 +13,7 @@ git fetch --depth=1 origin '+refs/heads/*:refs/remotes/origin/*'
 git checkout -B automation origin/automation
 git checkout origin/master -- GNUmakefile rootfs
 
-make SHELL=bash build/newmoon-3dnow/debain8gcc7+newmoon3dnow-32.3.1.linux-i586-gtk2.xz.sfs publish
+make SHELL=bash build/newmoon-3dnow/debain8gcc7+newmoon3dnow-32.3.1.linux-i586-gtk2.zstd.sfs publish
 
 find "${DIR}" -type f ! -size 0 -exec sh -euc 'for f in "${@}"; do : >"${f}"; done' sh '{}' +
 
