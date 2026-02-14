7c8fa750d55c8e163746b5f88da5b84549364849
qq542vev <qq542vev@yahoo.co.jp>
2026-02-14 19:55:57 +0900

    docs: コメントを追加。

--- automation.sh
+++ automation.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+### Script: automation.sh
+##
+## 自動実行用のスクリプト。
+##
+## Metadata:
+##
+##   id - 260ff918-6dc8-4fd1-8be9-4fd466c0fc5f
+##   author - <qq542vev at https://purl.org/meta/me/>
+##   version - 1.0.0
+##   created - 2026-02-14
+##   modified - 2026-02-14
+##   copyright - Copyright (C) 2026-2026 qq542vev. All rights reserved.
+##   license - <GPL-3.0-only at https://www.gnu.org/licenses/gpl-3.0.txt>
+##
+## See Also:
+##
+##   * <Project homepage at https://github.com/qq542vev/newmoon-sfs>
+##   * <Bag report at https://github.com/qq542vev/newmoon-sfs/issues>
+
 set -efuo pipefail
 
 readonly DIR='build'
@@ -15,12 +34,12 @@ git checkout origin/master -- GNUmakefile rootfs
 
 make SHELL=/bin/bash all publish
 
-find "${DIR}" -type f ! -size 0 -exec sh -euc 'for f in "${@}"; do : >"${f}"; done' sh '{}' +
+find -- "${DIR}" -type f ! -size 0 -exec sh -euc 'for f in "${@}"; do : >"${f}"; done' sh '{}' +
 
-case "$(git status --porcelain "${DIR}")" in
+case "$(git status --porcelain -- "${DIR}")" in
 	'') exit 0;;
 esac
 
-git add "${DIR}"
-git commit -m 'automation: mark processed files' "${DIR}"
-git push origin 'HEAD:automation'
+git add -- "${DIR}"
+git commit -m 'automation: mark processed files' -- "${DIR}"
+git push origin HEAD
